4790bd73c46a91b5c60044f847513ef46aa28a70
Dotnet9 <1012434131@qq.com>
2022-09-29 16:27:21 +0800

    添加日志打印和错误处理操作；

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"dotnet9.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Dotnet9")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	} 
+	
 	fmt.Println(message)
 }
